059781b943e606c75cf7a6e1d7c394b527d8a597
fabgarsan <fabiogarciasanchez@gmail.com>
2021-08-28 15:22:29 -0500

    :hammer: Refactor create mutant labmda function to use custom exceptions

--- handlers/create_mutant.py
+++ handlers/create_mutant.py
@@ -2,22 +2,30 @@ import json
 
 from utils.db import db
 from utils.functions import create_mutant
+from utils.custom_exceptions import CustomGeneralException
 
 
 def handler(event, context):
-    body = json.loads(event['body'])
-    dna = body.get('dna', None)
-    result = create_mutant(dna)
-    if result:
+    try:
+        db.begin_transaction()
+        body = json.loads(event['body'])
+        dna = body.get('dna', None)
+        result = create_mutant(dna)
+        db.commit()
+        if result:
+            response = {
+                "statusCode": 200,
+                "body": "It's Mutant"
+            }
+        else:
+            response = {
+                "statusCode": 403
+            }
+        return response
+    except CustomGeneralException as error:
+        db.rollback()
         response = {
-            "statusCode": 200,
-            "body": "It's Mutant"
+            "statusCode": error.status_code,
+            "body": json.dumps(error.message)
         }
-    else:
-        response = {
-            "statusCode": 403
-        }
-
-    db.begin_transaction()
-    db.commit()
-    return response
+        return response
