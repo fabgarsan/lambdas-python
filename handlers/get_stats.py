294dd912decaf61b886deefbc861e0183b1dc6c5
fabgarsan <fabiogarciasanchez@gmail.com>
2021-08-28 15:24:17 -0500

    :bug: Fix the result for get stats lambda function when count_mutant_dna and count_human_dna are None

--- handlers/get_stats.py
+++ handlers/get_stats.py
@@ -8,8 +8,8 @@ def handler(event, context):
     response = {
         "statusCode": 200,
         "body": json.dumps({
-            "count_mutant_dna": count_mutant_dna,
-            "count_human_dna": count_human_dna,
+            "count_mutant_dna": count_mutant_dna if count_mutant_dna is not None else 0,
+            "count_human_dna": count_human_dna if count_human_dna is not None else 0,
             "ratio": calculate_ratio(count_mutant_dna, count_human_dna)
         })
     }
